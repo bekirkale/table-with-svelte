5fd475a3506395ce1d5bcbba092e9594b428fb50
Bekir Kale <bekirkale@gmail.com>
2023-10-25 14:18:46 +0300

    Create inputBox.svelte

---
+++ src/routes/inputBox.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    let data
+    export let kind
+
+    function save (event){
+        if(event.keyCode==13){
+            dispatch('done', {
+            text:data,
+            })
+        }
+    }
+</script>
+
+    {#if kind =="q"}
+        Q:<input type="text" bind:value={data} on:keyup={save} autofocus >
+    {:else if kind == "a"}
+        A:<input type="text" bind:value={data} on:keyup={save} autofocus >
+    {/if}
+
