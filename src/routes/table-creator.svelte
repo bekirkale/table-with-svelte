bec4eda8dca9d5ac6b83cbcdeb53d5febcb6dea6
Bekir Kale <bekirkale@gmail.com>
2023-10-25 14:20:10 +0300

    Update table-creator.svelte

--- src/routes/table-creator.svelte
+++ src/routes/table-creator.svelte
@@ -4,8 +4,9 @@
 	let column
 </script>
 
-<input type="number" min=0 bind:value={row}>
-<input type="number" min=0 bind:value={column}>
+    <p>Select Rows and Columns to create a TABLE</p>
+    <input type="number" min=0 bind:value={row}>
+    <input type="number" min=0 bind:value={column}>
 
 {#if row>0 && column>0}
 	<Table r={row} c={column}/>
